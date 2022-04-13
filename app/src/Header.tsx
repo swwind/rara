3a14e3005cb0f7416290d10af514009fe595c68e
swwind <swwind233@gmail.com>
2022-04-13 15:01:19 +0800

    feat: add header title

--- app/src/Header.tsx
+++ app/src/Header.tsx
@@ -1,13 +1,15 @@
 import { useContext } from "react";
 import { ThemeContext } from "~/utils/context/theme";
 
+import metadata from "~/metadata.json";
+
 export default function Header() {
   const { theme, setTheme } = useContext(ThemeContext);
 
   return (
     <header className="rara-layout-header">
       <div className="rara-layout-header-container">
-        <div>你所热爱的，就是你的生活</div>
+        <div>{metadata.header}</div>
         <div>
           <span
             className="rara-layout-header-theme-switch"
